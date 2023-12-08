894bd88588b17a8278177eced44c90c86804b141
Matthias Kleine <github.com9@emailu.de>
2023-12-08 14:24:28 +0100

    Day 08

---
+++ day-08/index.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const fs = require('node:fs');
+
+const input = process.argv.length > 2 ? process.argv[2] : 'input';
+const text = String(fs.readFileSync(`./${input}.txt`));
+const rows = text.split('\n');
+
+const dirs = rows[0].split('');
+const nodes = {};
+
+for (let r = 2; r < rows.length; r++) {
+    const [ tmp, id, L, R ] = rows[r].match(/([0-9A-Z]{3}) = \(([0-9A-Z]{3}), ([0-9A-Z]{3})/);
+    nodes[id] = {L, R};
+}
+
+function runsTo(start, ending) {
+    let d = 0;
+    let pos = start;
+    let runs = 0;
+
+    do {
+        if (d == dirs.length) d = 0;
+
+        pos = nodes?.[pos]?.[dirs[d++]];
+        runs++;
+    } while (pos && !pos.endsWith(ending));
+
+    return runs;
+}
+
+// Round 1
+console.log('Round 1:');
+console.log(runsTo('AAA', 'ZZZ'));
+
+// Round 2
+/*
+const ghostNodes = Object
+    .keys(nodes)
+    .filter(n => n.endsWith('A'));
+
+let d = 0;
+let runs = 0;
+do {
+    runs++;
+
+    if (d == dirs.length) d = 0;
+    let dir = dirs[d++];
+
+    for (let g = 0; g < ghostNodes.length; g++) {
+        const newPos = nodes[ghostNodes[g]][dir];
+        if (!newPos) console.error(`${ghostNodes[g]} as no new pos ${dir}`) && process.exit();
+        //console.log(`${runs}: ${ghostNodes[g]} moves ${dir} to ${newPos}`);
+        ghostNodes[g] = newPos;
+    }
+
+    if (runs > 100) {
+        //process.exit();
+    }
+} while (ghostNodes.filter(n => n.endsWith('Z')).length !== ghostNodes.length);
+*/
+
+function traverse(node, endings) {
+    let steps = 0;
+    let d = 0;
+    let pos = node;
+
+    while (!endings.includes(pos)) {
+        if (d === dirs.length) d = 0;
+
+        pos = nodes[pos][dirs[d++]];
+        steps++;
+    }
+
+    return steps;
+}
+
+const startNodes = Object.keys(nodes).filter((n) => n.endsWith('A'));
+const endNodes = Object.keys(nodes).filter((n) => n.endsWith('Z'));
+
+function leastCommonMultiple(nums) {
+    const ggT = (a, b) => (!b ? a : ggT(b, a % b));
+    const kgV = (a, b) => (a * b) / ggT(a, b);
+
+    return nums.reduce((multiple, num) => kgV(multiple, num), Math.min(...nums));
+}
+
+const endingSteps = startNodes.map((n) => traverse(n, endNodes));
+const result2 = leastCommonMultiple(endingSteps);
+
+console.log('Round 2:');
+console.log(result2);
